61ecb7bcae080d4b13895e8dea8e163d58ffc4d1
AzanuLbizaR <almaz@kamaliev.ru>
2023-02-22 14:41:04 +0300

    add columns to about

---
+++ migrations/m230222_113633_add_columnt_to_about.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m230222_113633_add_columnt_to_about
+ */
+class m230222_113633_add_columnt_to_about extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m230222_113633_add_columnt_to_about cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m230222_113633_add_columnt_to_about cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
